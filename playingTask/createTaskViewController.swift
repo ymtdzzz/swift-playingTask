0285e64e5ddcfc0ea6a0f724ab6a7a9509bd0d7b
Y.Matsuda <zeroclock@icloud.com>
2019-03-03 15:33:18 +0900

    入力フォーム完了

---
+++ playingTask/createTaskViewController.swift
@@ -0,0 +1,129 @@
+//
+//  createTaskViewController.swift
+//  playingTask
+//
+//  Created by 松田陽佑 on 2019/03/03.
+//  Copyright © 2019 松田陽佑. All rights reserved.
+//
+
+import UIKit
+
+class createTaskViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    // タスクのタイトル
+    @IBOutlet weak var todoTitle: UITextField!
+    // タイマー
+    @IBOutlet weak var timePicker: UIDatePicker!
+    // 曜日選択
+    @IBOutlet weak var weekPicker: UITableView!
+    // ジャンル選択
+    @IBOutlet weak var categoryPicker: UIPickerView!
+    
+    // Done押下
+    @IBAction func tapDone(_ sender: Any) {
+        // 入力チェック
+        if todoTitle.text == "" || (selectedWeek[0] == false && selectedWeek[1] == false && selectedWeek[2] == false && selectedWeek[3] == false && selectedWeek[4] == false && selectedWeek[5] == false && selectedWeek[6] == false) {
+            let alert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
+            alert.title = "入力エラー"
+            alert.message = "全ての項目を入力して下さい。"
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+//        //Navigation Controllerを取得
+//        let nav = self.presentingViewController  as! UINavigationController
+//        //呼び出し元のView Controllerを遷移履歴から取得しパラメータを渡す
+//        let prevVc = nav.viewControllers[nav.viewControllers.count-1] as! taskTableViewController
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    // 曜日選択用リスト
+    let weekdays:[String] = ["月曜日","火曜日","水曜日","木曜日","金曜日","土曜日","日曜日"]
+    // 選択された曜日を保持（初期値は全てfalse）
+    var selectedWeek:[Bool] = [false, false, false, false, false, false, false]
+    
+    // ジャンル選択用リスト
+    let categories:[String] = ["家事","勉強","運動","外出","その他"]
+    // 選択されたジャンルを保持
+    var selectedCategory:String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        weekPicker.allowsMultipleSelection = true
+    }
+    
+    // MARK: - UITableViewDatasource
+    // セクションの個数
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    // セクションごとの行数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return weekdays.count
+    }
+    // セルを作成
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        let cellData = weekdays[(indexPath as NSIndexPath).row]
+        cell.textLabel?.text = cellData
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegate
+    // 選択時
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        // 選択内容を更新
+        selectedWeek[indexPath.row] = true
+        cell?.accessoryType = .checkmark
+    }
+    // 選択解除時
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        // 選択内容を更新
+        selectedWeek[indexPath.row] = false
+        cell?.accessoryType = .none
+    }
+    
+    // MARK: - UIPickerViewDelegate
+    // 選択肢を作成
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let item = categories[row]
+        return item
+    }
+    // 選択された内容をを取得
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // 現在選択されている行番号
+        let row = pickerView.selectedRow(inComponent: 0)
+        // 選択されている項目名を取得
+        let item = self.pickerView(pickerView, titleForRow: row, forComponent: 0)
+        selectedCategory = item!
+        print(item!)
+    }
+    
+    
+    // MARK: - UIPickerViewDataSource
+    // コンポーネント数を返却
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    // コンポーネント毎の行数
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categories.count
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // 入力チェック
+        if todoTitle.text == "" || (selectedWeek[0] == false && selectedWeek[1] == false && selectedWeek[2] == false && selectedWeek[3] == false && selectedWeek[4] == false && selectedWeek[5] == false && selectedWeek[6] == false) {
+            let alert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
+            alert.title = "入力エラー"
+            alert.message = "全ての項目を入力して下さい。"
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+
+}
---
+++ playingTask/taskTableViewController.swift
@@ -0,0 +1,160 @@
+//
+//  taskTableViewController.swift
+//  playingTask
+//
+//  Created by 松田陽佑 on 2019/03/03.
+//  Copyright © 2019 松田陽佑. All rights reserved.
+//
+
+import UIKit
+
+class taskTableViewController: UITableViewController {
+
+    // ToDoを格納する配列
+    var todoList = [MyTodo]()
+    
+    @IBOutlet var taskTableView: UITableView!
+    
+    // 追加ボタン
+    @IBAction func tapAdd(_ sender: Any) {
+        // 移動先のビューコントローラを参照
+        let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "createTask")
+        // トランジションの映像効果
+        nextVC?.modalTransitionStyle = .coverVertical
+        // シーンを移動
+        present(nextVC!, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 保存しているToDoを読み込む
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+// タスクを管理するクラス
+class MyTodo: NSObject,  NSSecureCoding {
+    static var supportsSecureCoding: Bool {
+        return true
+    }
+    
+    // Todoのタイトル
+    var todoTitle:String?
+    // 目標時間（秒数）
+    var willTime:Int?
+    // 実施時間（秒数）
+    var didTime:Int?
+    // ジャンル
+    var category:String?
+    
+    // 曜日
+    var monday:Bool = false
+    var tuesday:Bool = false
+    var wednesday:Bool = false
+    var thursday:Bool = false
+    var friday:Bool = false
+    var saturday:Bool = false
+    var sunday:Bool = false
+    
+    // コンストラクタ
+    override init() {
+        
+    }
+    
+    // デコード処理の実装
+    required init?(coder aDecoder: NSCoder) {
+        todoTitle = aDecoder.decodeObject(forKey: "todoTitle") as? String
+        category = aDecoder.decodeObject(forKey: "category") as? String
+        willTime = aDecoder.decodeInteger(forKey: "willTime")
+        didTime = aDecoder.decodeInteger(forKey: "didTime")
+        monday = aDecoder.decodeBool(forKey: "monday")
+        tuesday = aDecoder.decodeBool(forKey: "tuesday")
+        wednesday = aDecoder.decodeBool(forKey: "wednesday")
+        thursday = aDecoder.decodeBool(forKey: "thursday")
+        friday = aDecoder.decodeBool(forKey: "friday")
+        saturday = aDecoder.decodeBool(forKey: "saturday")
+        sunday = aDecoder.decodeBool(forKey: "sunday")
+    }
+    // エンコード処理
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(todoTitle, forKey: "todoTitle")
+        aCoder.encode(category, forKey: "category")
+        aCoder.encode(willTime, forKey: "willTime")
+        aCoder.encode(didTime, forKey: "didTime")
+        aCoder.encode(monday, forKey: "monday")
+        aCoder.encode(tuesday, forKey: "tuesday")
+        aCoder.encode(wednesday, forKey: "wednesday")
+        aCoder.encode(thursday, forKey: "thursday")
+        aCoder.encode(friday, forKey: "friday")
+        aCoder.encode(saturday, forKey: "saturday")
+        aCoder.encode(sunday, forKey: "sunday")
+    }
+}
